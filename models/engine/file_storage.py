0e4fa25b66386ec779c56637dd5111d6482da1e3
kevxcyj <kevcyjunior@gmail.com>
2024-07-09 21:23:35 -0500

    fixed indention

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,7 +65,7 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
     
-     def close(self):
+    def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
