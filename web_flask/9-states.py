dae7fdc8db4d2ddfba29ecc7d974835949a711e5
kevxcyj <kevcyjunior@gmail.com>
2024-07-09 23:05:08 -0500

    fixed pycode

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
 
+
 from flask import Flask, render_template
 from models import *
 from models import storage
@@ -18,6 +19,7 @@ def state(state_id=None):
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
