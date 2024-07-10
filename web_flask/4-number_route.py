48c4a5674102b758fcaf265341d77068362d1d11
kevxcyj <kevcyjunior@gmail.com>
2024-07-09 23:04:02 -0500

    fixed pycode

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
 
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -30,6 +31,7 @@ def pythoniscool(text='is cool'):
 def nisanumber(n):
     """ Displays n is a number only if n is an integer """
     return "{:d} is a number".format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
