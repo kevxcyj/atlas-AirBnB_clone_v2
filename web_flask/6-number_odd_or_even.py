4b5d0d8ca1f62222f440b7c00b309cecfac7c5e0
kevxcyj <kevcyjunior@gmail.com>
2024-07-09 23:04:35 -0500

    fixed pycode

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
 
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -44,6 +45,7 @@ def oddoreven(n):
         euen = 'odd'
     return render_template('6-number_odd_or_even.html', n=n,
                            euen=euen)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
